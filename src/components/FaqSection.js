0fd6490fa890480ebfc17b4d91826d97d63570a4
Anvarjon-Ibrohimov <anvarjon310594@gmail.com>
2024-09-05 17:58:22 +0500

    Faq section html part ended

---
+++ src/components/FaqSection.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+function FaqSection() {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>Can I learn programming from 0 on your platform?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae,
+            reprehenderit.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+            fuga veritatis? Explicabo recusandae natus dolorum!
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>How much I need to pay per months?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae,
+            reprehenderit.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
+            accusantium, facilis sint fugit ratione, amet corporis quidem
+            dolorem, quos eaque delectus repellat doloremque maiores aliquam
+            porro facere quam molestias cupiditate.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What kind of groups are there in your school?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae,
+            reprehenderit.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Is there any age range?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae,
+            reprehenderit.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt,
+            cupiditate?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,15 @@
 import React from "react";
+// components
 import AboutSection from "../components/AboutSection";
 import BenefitsSection from "../components/BenefitsSection";
+import FaqSection from "../components/FaqSection";
 
 function AboutUs() {
   return (
     <>
       <AboutSection />
       <BenefitsSection />
+      <FaqSection />
     </>
   );
 }
