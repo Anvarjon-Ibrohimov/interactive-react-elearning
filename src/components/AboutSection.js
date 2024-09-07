b96d890827bb341ce04b6a3c74d460c708a3d67d
Anvarjon-Ibrohimov <anvarjon310594@gmail.com>
2024-09-07 17:29:34 +0500

    motion design

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,14 +4,22 @@ import homeIntro from "../images/reading.avif";
 // import styled
 // import styled from "styled-components";
 import { About, Description, Hide, Image } from "../styles";
+// import About Motion
+import { motion } from "framer-motion";
 
 function AboutSection() {
+  const titleAnim = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 1.2 } },
+  };
   return (
     <About>
       <Description>
         <div className="title">
           <Hide>
-            <h2>Be educated so that</h2>
+            <motion.h2 variants={titleAnim} initial="hidden" animate="show">
+              Be educated so that
+            </motion.h2>
           </Hide>
           <Hide>
             <h2>
